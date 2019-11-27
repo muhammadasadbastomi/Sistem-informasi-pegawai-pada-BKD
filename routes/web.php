20e9ee455a89c7b0249dd2bb489511fad1597e54
tri angga utama <trianggategarutama@gmail.com>
2019-11-27 20:47:38 +0800

    dinamisasi template

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+//middleware auth
+Route::get('/admin/index', 'adminController@index')->name('adminIndex');
+//batas middleware auth
