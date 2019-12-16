ecd006475b524b425676ad8875dce6ca9454edc5
tri angga utama <trianggategarutama@gmail.com>
2019-12-16 20:26:23 +0800

    tambah script golongan

--- resources/views/admin/pangkat/index.blade.php
+++ resources/views/admin/pangkat/index.blade.php
@@ -30,7 +30,7 @@
               <div class="card-body">
                 <br>
                 <div class="card-body">
-                <table id="example1" class="table table-bordered table-striped">
+                <table id="datatable" class="table table-bordered table-striped">
                 <thead>
                 <tr>
                   <th>Pangkat / Golongan</th>
@@ -39,11 +39,7 @@
                 </tr>
                 </thead>
                 <tbody>
-                <tr>
-                  <td>III /b</td>
-                  <td>Pembina Utama Muda</td>
-                  <td class="text-center"><a href="" class="btn btn-primary"> <i class="fas fa-edit"></i> </a> <a href="" class="btn btn-danger"> <i class="fas fa-trash"></i> </a></td>
-                </tr>
+
                 </tbody>
                 <tfoot>
                 <tr>
@@ -74,8 +70,8 @@
             <div class="modal-body">
                 <form  method="post" action="">
                     <div class="form-group"><input type="hidden" id="id" name="id"  class="form-control"></div>
-                    <div class="form-group"><label  class=" form-control-label">Pangkat / Golongan</label><input type="text" id="nama_kelurahan" name="nama_kelurahan" placeholder="" class="form-control"></div>
-                    <div class="form-group"><label  class=" form-control-label">Keterangan</label><textarea name="" id="" class="form-control"></textarea></div>
+                    <div class="form-group"><label  class=" form-control-label">Kode Golongan</label><input type="text" id="kode_golongan" name="kode_golongan" placeholder="" class="form-control"></div
+                    <div class="form-group"><label  class=" form-control-label">Pangkat / Golongan</label><input type="text" id="golongan" name="golongan" placeholder="" class="form-control"></div>
             <div class="modal-footer">
                 <button type="button" class="btn " data-dismiss="modal"> <i class="ti-close"></i> Batal</button>
                 <button id="btn-form" type="submit" class="btn btn-primary"><i class="fasr fa-save"></i> </button>
@@ -89,23 +85,141 @@
 @endsection
 @section('script')
 <script>
+function hapus(uuid, nama){
+    var csrf_token=$('meta[name="csrf_token"]').attr('content');
+    Swal.fire({
+                title: 'apa anda yakin?',
+                text: " Menghapus  Data golongan " + nama,
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'hapus data',
+                cancelButtonText: 'batal',
+                reverseButtons: true
+            }).then((result) => {
+                if (result.value) {
+                    $.ajax({
+                        url : "{{ url('/api/golongan')}}" + '/' + uuid,
+                        type : "POST",
+                        data : {'_method' : 'DELETE', '_token' :csrf_token},
+                        success: function (response) {
+                            Swal.fire({
+                            position: 'top-end',
+                            icon: 'success',
+                            title: 'Data Berhasil Dihapus',
+                            showConfirmButton: false,
+                            timer: 1500
+                        })
+                    $('#datatable').DataTable().ajax.reload(null, false);
+                },
+            })
+            } else if (result.dismiss === swal.DismissReason.cancel) {
+                Swal.fire(
+                'Dibatalkan',
+                'data batal dihapus',
+                'error'
+                )
+            }
+        })
+    }
     $('#tambah').click(function(){
         $('.modal-title').text('Tambah Data');
-        $('#kd_kecamatan').val('');
-        $('#nama_kecamatan').val('');  
+        $('#kode_golongan').val('');
+        $('#golongan').val('');  
         $('#btn-form').text('Simpan Data');
         $('#mediumModal').modal('show');
     })
-  $(function () {
-    $("#example1").DataTable();
-    $('#example2').DataTable({
-      "paging": true,
-      "lengthChange": false,
-      "searching": false,
-      "ordering": true,
-      "info": true,
-      "autoWidth": false,
+    function edit(uuid){
+        $.ajax({
+            type: "GET",
+            url: "{{ url('/api/golongan')}}" + '/' + uuid,
+            beforeSend: false,
+            success : function(returnData) {
+                $('.modal-title').text('Edit Data');
+                $('#id').val(returnData.data.uuid);
+                $('#kode_golongan').val(returnData.data.kode_golongan);
+                $('#golongan').val(returnData.data.golongan);
+                $('#btn-form').text('Ubah Data');
+                $('#mediumModal').modal('show');
+            }
+        })
+    }
+$(document).ready(function() {
+    $('#datatable').DataTable( {
+        responsive: true,
+        processing: true,
+        serverSide: true,
+        searching : true,
+        paging    : true,
+        ajax: {
+            "type": "GET",
+            "url": "{{route('API.golongan.get')}}",
+            "dataSrc": "data",
+            "contentType": "application/json; charset=utf-8",
+            "dataType": "json",
+            "processData": true
+        },
+        columns: [
+            {"data": "kode_golongan"},
+            {"data": "golongan"},
+            {data: null , render : function ( data, type, row, meta ) {
+                var uuid = row.uuid;
+                var nama = row.nama;
+                return type === 'display'  ?
+                '<button onClick="edit(\''+uuid+'\')" class="btn btn-sm btn-outline-primary" data-toggle="modal" data-target="#editmodal"><i class="ti-pencil"> edit</i></button> <button onClick="hapus(\'' + uuid + '\',\'' + nama + '\')" class="btn btn-sm btn-outline-danger" > <i class="ti-trash">hapus</i></button>':
+            data;
+            }}
+        ]
     });
-  });
-</script>
+    $("form").submit(function (e) {
+        e.preventDefault()
+        var form = $('#modal-body form');
+        if($('.modal-title').text() == 'Edit Data'){
+            var url = '{{route("API.golongan.update", '')}}'
+            var id = $('#id').val();
+            $.ajax({
+                url: url+'/'+id,
+                type: "put",
+                data: $(this).serialize(),
+                success: function (response) {
+                    form.trigger('reset');
+                    $('#mediumModal').modal('hide');
+                    $('#datatable').DataTable().ajax.reload();
+                    Swal.fire({
+                        position: 'top-end',
+                        icon: 'success',
+                        title: 'Data Berhasil Tersimpan',
+                        showConfirmButton: false,
+                        timer: 1500
+                    })
+                },
+                error:function(response){
+                    console.log(response);
+                }
+            })
+        }else{
+            $.ajax({
+                url: "{{Route('API.golongan.create')}}",
+                type: "post",
+                data: $(this).serialize(),
+                success: function (response) {
+                    form.trigger('reset');
+                    $('#mediumModal').modal('hide');
+                    $('#datatable').DataTable().ajax.reload();
+                    Swal.fire({
+                        position: 'top-end',
+                        icon: 'success',
+                        title: 'Data Berhasil Disimpan',
+                        showConfirmButton: false,
+                        timer: 1500
+                    })
+                },
+                error:function(response){
+                    console.log(response);
+                }
+            })
+        }
+    } );
+    } );
+    </script>
 @endsection
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -139,7 +139,7 @@
                 </a>
               </li>
               <li class="nav-item">
-                <a href="#" class="nav-link ">
+                <a href="{{Route('jabatanIndex')}}" class="nav-link ">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Jabatan</p>
                 </a>
